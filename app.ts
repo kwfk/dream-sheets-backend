3952acf151014d7020345259379b978e36fab7b9
Frederick Kim <freddy.kim8@gmail.com>
2023-01-30 03:42:10 -0800

    add hash checking

--- app.ts
+++ app.ts
@@ -1,20 +1,46 @@
-import express from "express";
+import express, { Response } from "express";
 import dotenv from "dotenv";
 import path from "path";
 import fs from "fs";
+import md5 from "md5";
 import { generateImage } from "./image-generation";
 
 dotenv.config();
 const app = express();
 
-app.use(express.static("public"));
+app.use("/ftp", express.static("imgs"));
+
+const mime = {
+  jpg: "image/jpeg",
+  png: "image/png",
+};
+
+/**
+ * Read image file at filepath and serve it as the response
+ */
+const serveImage = (filepath: string, res: Response) => {
+  const ext = path.extname(filepath).slice(1);
+  let type = "text/plain";
+  if (ext === "jpg" || ext === "png") type = mime[ext];
+  const s = fs.createReadStream(filepath);
+  s.on("open", () => {
+    res.set("Content-Type", type);
+    s.pipe(res);
+  });
+  s.on("error", () => {
+    res.set("Content-Type", "text/plain");
+    res.status(404).end("Not found");
+  });
+};
 
 app.get("/", (req, res) => {
   const prompt = req.query.prompt;
-  console.log("prompt: ", prompt);
-  const files = fs.readdirSync(path.join(__dirname, "public"));
+  const files = fs.readdirSync(path.join(__dirname, "/../imgs"));
   const rand = Math.floor(Math.random() * files.length);
-  res.redirect(`/${files[rand]}`);
+
+  const img = path.join(__dirname, "..", "imgs", files[rand]);
+
+  serveImage(img, res);
 });
 
 app.get("/generate", async (req, res) => {
@@ -24,8 +50,24 @@ app.get("/generate", async (req, res) => {
     return;
   }
 
+  const hash = md5(prompt);
+
+  // check if image file with hash exists
+  const files = fs.readdirSync(path.join(__dirname, "..", "imgs"));
+  const cachedImage = files.find((f) => f.split(".")[0] === hash);
+  if (cachedImage) {
+    const cachedImagePath = path.join(__dirname, "..", "imgs", cachedImage);
+    serveImage(cachedImagePath, res);
+    return;
+  }
+
+  // generate new image with prompt
   const { seed, base64Image } = await generateImage(prompt);
   const img = Buffer.from(base64Image, "base64");
+  fs.writeFile(path.join(__dirname, "/../imgs", `${hash}.png`), img, (err) => {
+    if (err) throw err;
+    console.log("image has been saved");
+  });
 
   res.writeHead(200, {
     "Content-Type": "image/png",
